15e25192b026f1df4ce6c2bb4ede73932ec99961
Vitor Lamartine <vitorlamartine21@gmail.com>
2022-01-26 10:13:07 -0300

    Salvando o POST no Banco de Dados

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from lists.models import Item
 from django.shortcuts import render
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
